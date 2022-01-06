a30b2af3796fee92191c16afaaf54568577a26dd
lbyanista <mlabrayj@student.1337.ma>
2022-01-06 12:17:54 +0100

    m00ex00

---
+++ M00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#define cout std::cout
+#define ln std::endl
+
+int main(int ac, char **av)
+{
+    if (ac <= 1)
+    {
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << ln;
+        return 0;
+    }
+
+    for (int i = 1; i < ac; i++)
+    {
+       for (int j = 0; av[i][j]; j++)
+       {
+           cout << (char)toupper(av[i][j]);
+       }
+    }
+    cout << ln;
+    return 0;
+}
