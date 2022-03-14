0ae4941b4d4e4d6197b95d27371e0cafff56b461
Mohamed fadel Labrayji <mlabrayj@e2r8p11.1337.ma>
2022-03-14 19:21:13 +0100

    addfolderex05m01

---
+++ M01/ex05/Karen.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define ln std::endl
+#define string std::string
+#define cout std::cout
+
+
+class Karen
+{
+private:
+    /* data */
+public:
+    Karen(/* args */);
+    ~Karen();
+
+    void debug( void );
+    void info( void );
+    void warning( void );
+    void error( void );
+};
+
+Karen::Karen(/* args */)
+{
+}
+
+Karen::~Karen()
+{
+}
