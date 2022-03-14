8d7c16e374d313c09d3e45951f1dffe85bde0811
Mohamed fadel Labrayji <mlabrayj@e2r8p9.1337.ma>
2022-03-14 16:05:27 +0100

    addmaininex04m01

--- M01/ex04/main.cpp
+++ M01/ex04/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <fstream>
+// #include <cstdio>
 #define cout std::cout
 #define ln std::endl
 #define string std::string
 #define fstream std::fstream 
+#define ofstream std::ofstream
 
-int main(void)
+int main(int ac, char **av)
 {
-    string s1, s2;
-    fstream filetest;
-    filetest.open("test.txt");
-    filetest << "hello from here!";
-    filetest.close();
+    if(ac == 4)
+	{
+		fstream file(av[1], std::ios::in);
+    	string s1, s2;
+    	s1 = string(av[2]);
+    	s2 = string(av[3]);
+    	if(!file.is_open()) { cout << "cannot open this file" << ln; return 0; }
+    	if(s1.empty() || s2.empty()) { cout << "error: empty string" << ln; return 0; }
+		ofstream newfile(string(av[1]) + ".replace");
+		// while(file){
+			getline(file, s1);
+			size_t pos = 0;
+			pos = s1.find(av[2]);
+			if(pos >= 0 )
+				newfile << av[2];
+			if(pos == string::npos)
+				cout << "| " << av[2] << " |" << " not found in file: " << av[1] << ln ; return 0;
+			cout << pos << ln;
+		// }
+
+    }
+
+    else
+        cout << "error bad arguments" << ln ; return 0;
+
+    // filetest.close();
     
     return 0;
 }
