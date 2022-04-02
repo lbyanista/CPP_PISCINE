32c7d5f94b2225a87fe0c32808620a5ab5e8a116
Mohamed fadel Labrayji <mlabrayj@e2r9p8.1337.ma>
2022-04-02 19:04:47 +0000

    thingex02m04done

--- M04/ex02/Animal.hpp
+++ M04/ex02/Animal.hpp
@@ -9,18 +9,16 @@
 
 class Animal
 {
-    protected:
-        string type;
-
-    public:
-
-    Animal();
-    Animal(string type);
-    Animal(Animal const &an);
-    virtual ~Animal();
-    virtual Animal& operator=(const Animal &an);
-    virtual void makeSound() const;
-    string getType(void) const;
+protected:
+	string type;
+public:
+	Animal();
+	Animal(string type);
+	Animal(Animal const &an);
+	virtual ~Animal();
+	virtual Animal& operator=(const Animal &an);
+	virtual void makeSound(void) const = 0;
+	string getType(void) const;
 
 };
 
--- M04/ex02/main.cpp
+++ M04/ex02/main.cpp
@@ -2,59 +2,20 @@
 
 int main() {
 	// const Animal* meta = new Animal();
-	// const Animal* j = new Dog();
-	// const Animal* i = new Cat();
-	// cout << j->getType() << " " << ln;
-	// cout << i->getType() << " " << ln;
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	cout << j->getType() << " " << ln;
+	cout << i->getType() << " " << ln;
 	
 	// i->makeSound(); //will output the cat sound!
 	// j->makeSound();
 	// meta->makeSound();
 	// const WrongAnimal* k = new WrongCat();
 	// k->makeSound();
-	// delete k;
-	// delete j;
-
-	Animal *Ar[10];
+	delete i;
+	delete j;
 	
-	for (int i = 0; i < 10; i++)
-	{
-		if(i < 5)
-			Ar[i] = new Dog;
-		else
-			Ar[i] = new Cat;
-	}
-	
-	for (int i = 0; i < 10; i++)
-	{
-		delete Ar[i];
-	}
+	// Animal a;
 
-	Cat cat1, cat2;
-	Brain   *brain = cat1.GetBrain();
-    brain->ideas[0] = "I am the King !";
-    brain->ideas[1] = "OHHHH!!";
-
-    cout << " ------------- Get Ideas ------------ " << ln;
-
-    cout << cat1.getType() << " 1: " << cat1.GetBrain()->ideas[0] << ln;
-    cout << cat2.getType() << " 2: " << cat2.GetBrain()->ideas[0] << ln;
-
-    cout << " ------------- Assignement animal to animal ------------ " << ln;
-    cat2 = cat1;
-
-    cout << cat1.getType() << " 1: " << cat1.GetBrain()->ideas[0] << ln;
-    cout << cat2.getType() << " 2: " << cat2.GetBrain()->ideas[0] << ln;
-
-    cout << " ------------- Delete cats ------------ " << ln;
-
-
-	const Animal* p = new Dog();
-	const Animal* n = new Cat();
-	cout << p->getType() << ln;
-	delete p;//should not create a leak delete i;
-	delete n;
-
-	
 	return 0;
 }
