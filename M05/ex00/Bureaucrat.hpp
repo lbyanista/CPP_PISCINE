4489be43ff95051aab78fa1d8e89fa603fc0c423
Mohamed fadel Labrayji <mlabrayj@e2r8p11.1337.ma>
2022-04-10 02:54:08 +0000

    someofex00m05

--- M05/ex00/Bureaucrat.hpp
+++ M05/ex00/Bureaucrat.hpp
@@ -2,23 +2,37 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
-#include "Bureaucrat.hpp"
+#include <exception>
 #define cout std::cout
 #define ln std::endl
 #define string std::string
+#define exception std::exception
 
 class Bureaucrat
 {
 private:
     string const _name;
-    int _grand;
+    int _grade;
 public:
     Bureaucrat();
+    Bureaucrat(string name, int grade);
     Bureaucrat(Bureaucrat const &br);
     Bureaucrat & operator=(const Bureaucrat &br);
     ~Bureaucrat();
-    string getName(void);
-    int getGrade(void);
+
+    const string &getName(void)const;
+    const int &getGrade(void)const;
+
+    void    incGrade(void);
+    void    decGrade(void);
+
+    class GradeTooHighException : public exception {
+        const char * what () const throw ();
+    };
+    
+    class GradeTooLowException : public exception {
+        const char * what () const throw ();
+    };
 };
     std::ostream & operator<<(std::ostream & o, const Bureaucrat &br);
 
---
+++ M05/ex00/main.cpp
@@ -0,0 +1,21 @@
+#include "Bureaucrat.hpp"
+
+int main(void){
+
+    try{    
+        // ---------- Create Bureaucrat -------------- //
+        Bureaucrat br("Abdellah", 10); // if Grade < 1 OR Grade > 150 will catch an excep
+        // -------------- incrementGrade ------------ //
+        br.incGrade();
+        // ------------ decrementGrade -------------- //
+        br.decGrade();
+        br.decGrade();
+        // ----------  Operator Overload << --------- //
+        cout << br << ln;
+    }
+    catch (exception &e){
+        cout << "Exception : " << e.what() << ln; 
+    }
+
+    return 0;
+}
